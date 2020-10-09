c03e342e5b308ad41e680a0f61f8fdd47d1feef7
Tawnee Allport <allprt89@gmail.com>
2020-10-08 21:26:42 -0600

    make dark mode persistent

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { ThemeProvider } from "styled-components"
+import storage from "local-storage-fallback"
+
 import Header from "@components/Header"
 import Footer from "@components/Footer"
 
 import { GlobalStyle } from "@globalStyles"
 
+const getInitialTheme = () => {
+  const savedTheme = storage.getItem('isDark')
+  return savedTheme ? JSON.parse(savedTheme) : true
+}
+
 const Layout = ({ children }) => {
 
-  const [isDark, toggleTheme] = useState(true);
+  const [isDark, toggleTheme] = useState(getInitialTheme)
+
+  useEffect(() => {
+    storage.setItem('isDark', isDark)
+  }, [isDark])
 
   return (
     <>
